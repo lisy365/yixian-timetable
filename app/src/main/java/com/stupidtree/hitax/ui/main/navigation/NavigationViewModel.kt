894916fdec78aa3f07fc43580e01f589f7c0103a
此生挚爱凌波丽 <2788410557@qq.com>
2026-09-22 13:30:54 +0800

    add app/src/main/java/com/stupidtree/hitax/ui/main/navigation/NavigationViewModel.kt

---
+++ app/src/main/java/com/stupidtree/hitax/ui/main/navigation/NavigationViewModel.kt
@@ -0,0 +1,42 @@
+package com.stupidtree.hitax.ui.main.navigation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.switchMap
+import com.stupidtree.component.data.DataState
+import com.stupidtree.component.data.Trigger
+import com.stupidtree.hita.theta.data.repository.MessageRepository
+import com.stupidtree.hitax.data.model.timetable.Timetable
+import com.stupidtree.hitax.data.repository.TimetableRepository
+import com.stupidtree.stupiduser.data.repository.LocalUserRepository
+
+class NavigationViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val timetableRepository = TimetableRepository.getInstance(application)
+    private val localUserRepository = LocalUserRepository.getInstance(application)
+    private val messageRepo = MessageRepository.getInstance(application)
+
+    private val recentTimetableController = MutableLiveData<Trigger>()
+    val recentTimetableLiveData: LiveData<Timetable?> = recentTimetableController.switchMap {
+            return@switchMap timetableRepository.getRecentTimetable()
+        }
+    val timetableCountLiveData: LiveData<Int> = recentTimetableController.switchMap {
+            return@switchMap timetableRepository.getTimetableCount()
+        }
+
+
+    val unreadMessageLiveData: LiveData<DataState<Int>> = recentTimetableController.switchMap {
+            val lu = localUserRepository.getLoggedInUser()
+            if (lu.isValid()) {
+                return@switchMap messageRepo.countUnread(lu.token!!, "all")
+            } else {
+                return@switchMap MutableLiveData(DataState(DataState.STATE.NOT_LOGGED_IN))
+            }
+        }
+
+    fun startRefresh() {
+        recentTimetableController.value = Trigger.actioning
+    }
+}
